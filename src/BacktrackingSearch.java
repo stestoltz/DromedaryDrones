cc5debae90d5d0b74d7de990ff8c8b6826b31db1
Stephen S <stestoltz@users.noreply.github.com>
2020-03-14 12:20:25 -0400

    Stub classes

--- src/BacktrackingSearch.java
+++ src/BacktrackingSearch.java
@@ -1,4 +1,16 @@
+import java.util.List;
 
 public class BacktrackingSearch implements RoutingAlgorithm {
+
+	/**
+	 * given an unordered list of orders, order them and make a drone trip
+	 * 		in this case, use backtracking to efficiently do TSP
+	 * @param orders
+	 * @return drone trip containing orders, sequenced to be efficient
+	 */
+	@Override
+	public DroneTrip createTrip(List<Order> orders) {
+		
+	}
 
 }
--- src/RoutingAlgorithm.java
+++ src/RoutingAlgorithm.java
@@ -1,4 +1,22 @@
+import java.util.List;
 
 public interface RoutingAlgorithm {
 
+	/**
+	 * given an unordered list of orders, order them and make a drone trip
+	 * @param orders
+	 * @return drone trip containing orders, sequenced to be efficient
+	 */
+	public DroneTrip createTrip(List<Order> orders);
+	
+	/**
+	 * Calculates the distance between two points
+	 * @param a the first point
+	 * @param b the second point
+	 * @return the distance between the two points
+	 */
+	default double distance(DeliveryPoint a, DeliveryPoint b) {
+		
+	}
+	
 }
