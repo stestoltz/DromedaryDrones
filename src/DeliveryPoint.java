8bf9c1c1ab6712343553c7da1e2e84922c884558
Stephen S <stestoltz@users.noreply.github.com>
2020-03-16 10:17:59 -0400

    finished delivery point class

--- src/DeliveryPoint.java
+++ src/DeliveryPoint.java
@@ -1,9 +1,9 @@
 
 public class DeliveryPoint {
 	
-	int x;
-	int y;
-	String name;
+	private int x;
+	private int y;
+	private String name;
 	
 	/**
 	 * constructor
@@ -16,21 +16,26 @@ public class DeliveryPoint {
 		this.y = y;
 		this.name = name;
 	}
+	
 	public int getX() {
 		return x;
 	}
 	public void setX(int x) {
 		this.x = x;
 	}
+	
 	public int getY() {
 		return y;
 	}
+	
 	public void setY(int y) {
 		this.y = y;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
