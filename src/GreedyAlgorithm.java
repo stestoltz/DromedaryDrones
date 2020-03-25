9536c8c24314e7bb62e5ffb94128333dea656b8e
shaffernc17 <38110014+shaffernc17@users.noreply.github.com>
2020-03-25 11:59:42 -0400

    Update GreedyAlgorithm
    Made a createTrip method that calculates a greedy approach to creating a DroneTrip

--- src/GreedyAlgorithm.java
+++ src/GreedyAlgorithm.java
@@ -1,29 +1,37 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 public class GreedyAlgorithm implements RoutingAlgorithm{
-
-	@Override
-	public DroneTrip createTrip(List<Order> orders) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
+	@Override
 	/**
-	 * Sorts the array and then calculates the shortest distance between the different points
-	 * @param ary the array of x and y coordinates
-	 * @param n the number of times the loop is run calculating the distance between the points
-	 * @return the sum of the distance of one axis
+	 * Method calculates a greedy approach to creating a DroneTrip
+	 * @param List<Order> orders list of orders
+	 * @param home the location's home delivery point
+	 * @return a new DroneTrip with a sequence of Orders 
 	 */
-	public int greedyAlgorithm(int ary[], int n) {
-		Arrays.sort(ary);
-		
-		int result = 0, sum = 0;
-		for(int i = 0; i < n; i++) {
-			result += (ary[i] * i - sum);
-			sum += ary[i];
+	public DroneTrip createTrip(List<Order> orders, DeliveryPoint home) {
+		List<Order> tripList = new ArrayList<>();
+		DeliveryPoint previous = home;
+		// runs until the order list is empty
+		while(!orders.isEmpty()) {
+			double minDistance = Double.MAX_VALUE;
+			Order closest = null;
+			// looks at each order still left in the list and finds the closest order
+			for(Order o : orders) {
+				double distance = distance(o.getDeliveryPoint(), previous);
+				// updates the value of closest to o and the minDistance to distance
+				if(distance < minDistance) {
+					closest = o;
+					minDistance = distance;
+				}
+			}
+			tripList.add(closest);
+			orders.remove(closest);
+			previous = closest.getDeliveryPoint();
 		}
-		return result;
+		return new DroneTrip((Order[]) tripList.toArray());
 	}
 
 }
